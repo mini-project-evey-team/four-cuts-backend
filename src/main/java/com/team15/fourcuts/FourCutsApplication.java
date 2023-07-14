783d5f9cdeef550a3d58755f6e205e1d102cdf01
chaewon <cwlee4633@naver.com>
2023-07-14 13:23:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/team15/fourcuts/FourCutsApplication.java
@@ -0,0 +1,13 @@
+package com.team15.fourcuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourCutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourCutsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team15/fourcuts/FourCutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team15.fourcuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourCutsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
